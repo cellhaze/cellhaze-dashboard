bdb6337b5ed6cff853464e7015f27c48608fc6fe
Cell <marcell.made@gmail.com>
2026-02-11 17:02:13 -0500

    Initial dashboard setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ActiveThemeProvider } from "@/components/active-theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +21,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ActiveThemeProvider>{children}</ActiveThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
